5ecf8f5f8b1556f3ad6f629f16bfe46e029ff413
candidtim <timcandid@gmail.com>
2016-10-03 23:43:54 +0200

    Problem 19 bits and pieces

--- src/Problem19.hs
+++ src/Problem19.hs
@@ -1,7 +1,45 @@
 module Problem19
   ( countSundaysOnFirst
+  , addDays
+  , nextDay
+  , leap
   ) where
+
+
+type Day = Int
+type Month = Int
+type Year = Int
+type Date = (Day,Month,Year)
+
+jan = 1
+feb = 2
+mar = 3
+apr = 4
+may = 5
+jun = 6
+jul = 7
+aug = 8
+sep = 9
+oct = 10
+nov = 11
+dec = 12
 
 
 countSundaysOnFirst :: Int
 countSundaysOnFirst = 0
+
+addDays :: Date -> Int -> Date
+addDays date days = iterate nextDay date !! days
+
+nextDay :: Date -> Date
+nextDay (day, month, year)
+  | day == 28 && month == feb && (not.leap $ year) = (1, mar, year)
+  | day == 28 && month == feb = (29, feb, year)
+  | day == 29 && month == feb = (1, mar, year)
+  | day == 30 && (month == apr || month == jun || month == sep || month == nov) = (1, month+1, year)
+  | day == 31 && month == dec = (1, jan, year+1)
+  | day == 31 = (1, month+1, year)
+  | otherwise = (day+1, month, year)
+
+leap :: Year -> Bool
+leap year = year `mod` 400 == 0 || (year `mod` 4 == 0 && year `mod` 100 /= 0)
--- test/Problem19Tests.hs
+++ test/Problem19Tests.hs
@@ -5,8 +5,32 @@ module Problem19Tests
 
 import Test.HUnit
 
+import Problem19
 
 
+testLeapYear :: Test
+testLeapYear = TestCase $ do
+  assertBool "1900 was not leap (divides by 100 and not by 400)" (not.leap $ 1900)
+  assertBool "1904 was leap" (leap 1904)
+  assertBool "1980 was leap" (leap 1980)
+  assertBool "2000 was leap (divides by 400)" (leap 2000)
+
+testNextDay :: Test
+testNextDay = TestCase $ do
+  assertEqual "28/02/1999 -> 01/03/1999" (1, 3, 1999) (nextDay (28, 2, 1999))
+  assertEqual "28/02/2000 -> 29/02/2000" (29, 2, 2000) (nextDay (28, 2, 2000))
+  assertEqual "29/02/2000 -> 01/03/2000" (1, 3, 2000) (nextDay (29, 2, 2000))
+  assertEqual "30/04/1999 -> 01/05/1999" (1, 5, 1999) (nextDay (30, 4, 1999))
+  assertEqual "30/05/1999 -> 31/05/1999" (31, 5, 1999) (nextDay (30, 5, 1999))
+  assertEqual "31/05/1999 -> 01/06/1999" (1, 6, 1999) (nextDay (31, 5, 1999))
+  assertEqual "22/05/1999 -> 23/05/1999" (23, 5, 1999) (nextDay (22, 5, 1999))
+  assertEqual "31/12/1999 -> 01/01/2000" (1, 1, 2000) (nextDay (31, 12, 1999))
+
+testAddDays :: Test
+testAddDays = TestCase $ do
+  assertEqual "30/05/1999 + 2 days" (1, 6, 1999) (addDays (30, 5, 1999) 2)
+  assertEqual "22/05/1997 + 365 days" (22, 5, 1998) (addDays (22, 5, 1997) 365)
+  assertEqual "22/05/1999 + 365 days" (21, 5, 2000) (addDays (22, 5, 1999) 365)
 
 
-problem19Tests = TestList []
+problem19Tests = TestList [ testLeapYear, testNextDay, testAddDays ]
